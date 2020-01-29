3d161dcd8a5df7fc32d9c6dda52483872e7f2bfc
MD. Emtazul Haque <shifat63csedu@gmail.com>
2020-01-29 05:34:00 +0100

    XML feature added alongside JSON

--- src/test/java/com/shifat63/springmvcrestapi/controllers/v1/ConvertToXML.java
+++ src/test/java/com/shifat63/springmvcrestapi/controllers/v1/ConvertToXML.java
@@ -1,14 +1,14 @@
 package com.shifat63.springmvcrestapi.controllers.v1;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 
 // Author: Shifat63
 
-public abstract class ConvertToJson {
-    public static String asJsonString(final Object obj) {
+public abstract class ConvertToXML {
+    public static String asXmlString(final Object obj) {
         try {
-            ObjectMapper objectMapper = new JsonMapper();
+            ObjectMapper objectMapper = new XmlMapper();
             return objectMapper.writeValueAsString(obj);
         } catch (Exception e) {
             throw new RuntimeException(e);
